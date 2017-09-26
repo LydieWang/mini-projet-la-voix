ecf2d7f0c4c90f8625d1a93ee99365a4c3fae8e9
Blanca <wtxlz@163.com>
2017-09-27 00:47:40 +0200

    Ajouter la fonction d'entregistrer la voix par microphone.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# mini-projet-la-voix
+﻿# mini-projet-la-voix
 L’objectif ici est de développer une application mobile sur android (java), permettant d’enregistrer la voix et ensuite de l’analyser. La partie analyse sera réalisée grâce à des formules mathématiques qui seront données. Le cahier de spécification est disponible, restera donc la partie développement et tests. Ce travail sera déroulera dans un contexte médical et le client en sera un clinicien hospitalier  de l’hôpital Bretonneau. 
+
+26/09/2017 Tianxue
+Ajouter la fonction d'enregistrer la voix par microphone.
+[AndroidManifest.xm]
+[RecordActivity.java]
+[RecordActivity.xml]
--- Voix_Analyse/app/src/main/java/com/polytech/di/tianxue/voix_analyse/RecordActivity.java
+++ Voix_Analyse/app/src/main/java/com/polytech/di/tianxue/voix_analyse/RecordActivity.java
@@ -1,5 +1,8 @@
 package com.polytech.di.tianxue.voix_analyse;
 
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,21 +10,81 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+
 public class RecordActivity extends AppCompatActivity {
+
+    private TextView textView_hint;
+    private RelativeLayout layout;
+    private Button button_start;
+    private Button button_stop;
+    private MediaRecorder mediaRecorder;
+    private File dirSD;
+    private boolean isCardMonted;
+    private File recAudioFile;
+    private String filePath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        init();
+    }
+
+    private void init(){
+        layout = (RelativeLayout)findViewById(R.id.content);
+        button_start = (Button)findViewById(R.id.button_start);
+        button_stop = (Button)findViewById(R.id.button_stop);
+        button_start.setEnabled(true);
+        button_stop.setEnabled(false);
+        isCardMonted = false;
     }
 
     public void startRecording(View view){
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText("STARTING");
-        RelativeLayout layout = (RelativeLayout)findViewById(R.id.content);
-        layout.addView(textView);
-        Button button = (Button)findViewById(R.id.button_record);
-        button.setVisibility(view.INVISIBLE);
+        textView_hint = new TextView(this);
+        textView_hint.setTextSize(40);
+        textView_hint.setText("STARTING");
+        layout.addView(textView_hint);
+        button_start.setEnabled(false);
+        button_stop.setEnabled(true);
+
+        isCardMonted = Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+
+        if(isCardMonted) {
+            dirSD = Environment.getExternalStorageDirectory();
+            filePath = dirSD.getPath();
+        }
+
+        try {
+            recAudioFile = new File(filePath, "Nouveau Enregistrement.amr");
+            if (recAudioFile.exists()) {
+                recAudioFile.delete();
+            }
+            mediaRecorder = new MediaRecorder();
+
+            /* 设置录音来源为麦克风 */
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+            //文件保存位置
+            mediaRecorder.setOutputFile(recAudioFile.getAbsolutePath());
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopRecording(View view){
+        textView_hint.setVisibility(view.INVISIBLE);
+        button_stop.setEnabled(false);
+
+        if(recAudioFile != null) {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+        }
     }
 }
